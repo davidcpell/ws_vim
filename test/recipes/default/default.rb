bb299c068db884a479d96dd8d567df76ed0b9876
David Pell <davidcpell@gmail.com>
2016-08-13 17:40:45 -0400

    add test for centos

--- test/recipes/default/default.rb
+++ test/recipes/default/default.rb
@@ -1,3 +1,9 @@
-describe package('vim') do 
-  it { should be_installed }
+if os[:family] == 'centos'
+  describe package('vim-enhanced') do 
+    it { should be_installed }
+  end
+else
+  describe package('vim') do 
+    it { should be_installed }
+  end
 end
