3e9b2135ddb89ddbc6d055836a8bc54630e8c74c
David Pell <davidcpell@gmail.com>
2016-08-13 21:50:52 -0400

    fix rubocop offenses

--- test/recipes/default/default_test.rb
+++ test/recipes/default/default_test.rb
@@ -1,9 +1,9 @@
 if os[:family] == 'centos'
-  describe package('vim-enhanced') do 
+  describe package('vim-enhanced') do
     it { should be_installed }
   end
 else
-  describe package('vim') do 
+  describe package('vim') do
     it { should be_installed }
   end
 end
