9f9d97c85d9b0a64bcea03282b9ea3ccfae04c6a
David Pell <davidcpell@gmail.com>
2016-09-27 10:13:51 -0400

    install my plugins using plugins recipe

--- recipes/pathogen.rb
+++ recipes/pathogen.rb
@@ -1,1 +1,45 @@
 include_recipe 'pathogen'
+
+pathogen_plugin 'rainbow' do 
+  github_org 'luochen1990'
+end
+
+pathogen_plugin 'ctrlp.vim' do 
+  github_org 'ctrlpvim'
+end
+
+pathogen_plugin 'vim-bundler' do 
+  github_org 'tpope'
+end
+
+pathogen_plugin 'vim-commentary' do 
+  github_org 'tpope'
+end
+
+pathogen_plugin 'vim-endwise' do 
+  github_org 'tpope'
+end
+
+pathogen_plugin 'vim-hashicorp-tools' do 
+  github_org 'hashivim'
+end
+
+pathogen_plugin 'vim-rails' do 
+  github_org 'tpope'
+end
+
+pathogen_plugin 'vim-rspec' do 
+  github_org 'thoughtbot'
+end
+
+pathogen_plugin 'vim-ruby' do 
+  github_org 'vim-ruby' 
+end
+
+pathogen_plugin 'vim-surround' do 
+  github_org 'tpope'
+end
+
+pathogen_plugin 'vim-tmux-navigator' do 
+  github_org 'christoomey'
+end
--- test/recipes/default/pathogen_test.rb
+++ test/recipes/default/pathogen_test.rb
@@ -1,3 +1,15 @@
 describe file(File.join('/home', 'david', '.vim', 'autoload', 'pathogen.vim')) do 
   it { should be_file }
 end
+
+plugins = %w(
+  ctrlp.vim rainbow vim-bundler vim-commentary vim-endwise vim-hashicorp-tools vim-rails vim-rspec 
+  vim-ruby vim-surround vim-tmux-navigator
+)
+
+plugins.each do |plugin|
+  describe directory("/home/david/.vim/bundle/#{plugin}") do 
+    it { should be_directory }
+  end
+end
+
